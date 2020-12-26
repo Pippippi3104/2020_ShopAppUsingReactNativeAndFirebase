9a029651c670ada0cdfa02ccea28ad211983b674
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-26 16:04:14 +0900

    # finish editing lesson37

--- src/Section06/src/screens/CreateReviewScreen.tsx
+++ src/Section06/src/screens/CreateReviewScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useContext, useLayoutEffect, useState } from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import firebase from "firebase";
 import {addReview} from "../lib/firebase";
+import { pickImage } from "../lib/image-picker";
 import {UserContext} from "../contexts/userContexts";
 /* components */
 import { IconButton } from "../components/IconButton";
@@ -27,6 +28,7 @@ export const CreateReviewScreen: React.FC<Props> = ({
   const [text, setText] = useState<string>("");
   const [score, setScore] = useState<number>(3);
   const {user} = useContext(UserContext);
+  const [imageUri, setImageUri] = useState<string>("");
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -55,6 +57,11 @@ export const CreateReviewScreen: React.FC<Props> = ({
     await addReview(shop.id, review)
   };
 
+  const onPickImage = async () =>{
+    const uri = await pickImage();
+    setImageUri(uri);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StarInput score={score} onChangeScore={(value) => setScore(value)} />
@@ -64,6 +71,10 @@ export const CreateReviewScreen: React.FC<Props> = ({
       　label="レビュー"
       　placeholder="レビューを書いてください"
       />
+      <View style={styles.container}>
+         <IconButton name="camera" onPress={onPickImage} color="#ccc" />
+         {!!imageUri && <Image source={{uri: imageUri}} style={styles.image} />}
+      </View>
       <Button text="レビューを投稿する" onPress={onSubmit} />
     </SafeAreaView>
   );
@@ -73,5 +84,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  photoCOntainer: {
+    margin: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 8,
   },
 });
